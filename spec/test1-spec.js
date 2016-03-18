e91106f3adcd743a25fa9b25afc4c82344900335
thomp201 <louis.t@hotmail.co.uk>
2016-03-18 05:27:20 +0000

    timetable added

--- spec/test1-spec.js
+++ spec/test1-spec.js
@@ -1,12 +1,1 @@
-const url = 'https://theta-website-athelvaci.c9users.io/index.html';
-casper.test.begin('Start Bootstrap', 5, function suite(test) {
-    
-    casper.start(url, function() {
-    test.assertEquals(this.getTitle(), 'Bulk Order Calculator', 'check page title text')
-    test.assertVisible('fieldset', 'check form is visible');
-    test.assertExists('fieldset > p', 'check form title is present');
-    test.assertSelectorHasText('fieldset > p', 'Calculate a bulk book order.', 'check form title text');
-   
-  });
-    
-});
+describe('Maps API co-ord')
